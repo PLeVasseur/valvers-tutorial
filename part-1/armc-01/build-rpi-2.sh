d62d1550e52a157770fd9487e17dad80591b385d
Unknown <plevasseur@gmail.com>
2016-08-21 09:59:07 -0400

    Updated part-1/armc-01/build-rpi-2.sh to tell the linker to use -nostartfiles

--- part-1/armc-01/build-rpi-2.sh
+++ part-1/armc-01/build-rpi-2.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/local/gcc-arm-none-eabi-4_7-2013q3/bin/arm-none-eabi-gcc -O0 -mfpu=neon-vfpv4 -mfloat-abi=hard -march=armv7-a -mtune=cortex-a7 -g armc-01.c -o kernel.elf
+/usr/local/gcc-arm-none-eabi-4_7-2013q3/bin/arm-none-eabi-gcc -O0 -mfpu=neon-vfpv4 -mfloat-abi=hard -march=armv7-a -mtune=cortex-a7 -nostartfiles -g armc-01.c -o kernel.elf
